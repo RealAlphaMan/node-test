880229f997d5b3c239fd48080ee1bebd2672c805
LAP02173 <lap02173@LAP02173s-MacBook-Pro.local>
2022-05-04 16:21:44 +0700

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from ' + os.hostname());
+  res.end('Hello World from hehe ' + os.hostname());
 });
 
 server.listen(port, hostname, () => {
